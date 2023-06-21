fb79fc57275ececd5801462274b4e15b5d1012ec
unknown <harshika0102@gmail.com>
2023-06-21 18:55:30 +0530

    Wrote some code for file

---
+++ decode_morse_code.rb
@@ -0,0 +1,43 @@
+@items = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(character)
+  @items[character]
+end
+
+def decode_word(word)
+  letters = word.split
+  letters.map { |letter| decode_char(letter) }.join
+end
+
+def decode_message(message)
+  words = message.split('   ')
+  words.map { |word| decode_word(word) }.join(' ')
+end
+#messagedecoding
